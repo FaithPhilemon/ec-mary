1b03acd617c80967a7f00c4fe064606edf5561b2
Faith Philemon <philemonfaith@gmail.com>
2018-07-12 15:33:27 +0100

    Initial commit

---
+++ application/logs/log-2018-07-12.php
@@ -0,0 +1,63 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-07-12 14:28:17 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\ci-cart-master\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-07-12 14:28:17 --> Unable to connect to the database
+ERROR - 2018-07-12 14:38:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\ecommerce-mary\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-07-12 14:38:28 --> Unable to connect to the database
+ERROR - 2018-07-12 14:39:26 --> Severity: 8192 --> __autoload() is deprecated, use spl_autoload_register() instead C:\xampp\htdocs\ecommerce-mary\application\libraries\PHPMailer\PHPMailerAutoload.php 45
+ERROR - 2018-07-12 15:03:52 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:04:33 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:04:45 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:04:49 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:06:25 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:08:57 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:08:57 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:08:57 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:08:57 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:08:57 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:09:00 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:09:01 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:14:12 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:14:20 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:14:25 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:18:11 --> Severity: error --> Exception: syntax error, unexpected '?>' C:\xampp\htdocs\ecommerce-mary\application\views\templates\redlabel\_parts\footer.php 81
+ERROR - 2018-07-12 15:18:43 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:19:15 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:19:23 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:42:05 --> 404 Page Not Found: ../modules/vendor/controllers//index
+ERROR - 2018-07-12 15:42:09 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:42:19 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:43:38 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:57:50 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:58:29 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:59:21 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 15:59:54 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:00:13 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:02:39 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:02:39 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:02:39 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:02:53 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:02:59 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:02:59 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:03:00 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:08:04 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:20:21 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:20:21 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:20:22 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:20:50 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:22:36 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:22:36 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:22:36 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:22:36 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:22:36 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:22:38 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:22:40 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:23:41 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:23:41 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:23:41 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:24:31 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:24:32 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:24:32 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:24:59 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:25:00 --> 404 Page Not Found: /index
+ERROR - 2018-07-12 16:25:00 --> 404 Page Not Found: /index
